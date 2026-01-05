968d2476acb969f6ca73552989211a71de990ef0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-01-05 01:36:00 -0800

    add TestObjectStreamForArray

--- src/homework/TestObjectInputStream.java
+++ src/homework/TestObjectInputStream.java
@@ -1,20 +1,20 @@
-package homework;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-
-public class TestObjectInputStream {
-    public static void main(String[] args)
-        throws ClassNotFoundException, IOException {
-        try (
-                ObjectInputStream inputStream =
-                        new ObjectInputStream(new FileInputStream("object.dat"));
-                ){
-            String name = inputStream.readUTF();
-            double score = inputStream.readDouble();
-            java.util.Date date = (java.util.Date)(inputStream.readDouble());
-            System.out.println(name + "  " + score + "  " + date);
-        }
-    }
-}
+//package homework;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.io.ObjectInputStream;
+//
+//public class TestObjectInputStream {
+//    public static void main(String[] args)
+//        throws ClassNotFoundException, IOException {
+//        try (
+//                ObjectInputStream inputStream =
+//                        new ObjectInputStream(new FileInputStream("object.dat"));
+//                ){
+//            String name = inputStream.readUTF();
+//            double score = inputStream.readDouble();
+//            java.util.Date date = (java.util.Date)(inputStream.readDouble());
+//            System.out.println(name + "  " + score + "  " + date);
+//        }
+//    }
+//}
---
+++ src/homework/TestObjectStreamForArray.java
@@ -0,0 +1,34 @@
+package homework;
+
+import java.io.*;
+
+public class TestObjectStreamForArray {
+    public static void main(String[] args)
+        throws ClassNotFoundException, IOException {
+        int[] numbers = {1, 2, 3, 4, 5};
+        String[] strings = {"John", "Susan", "Kim"};
+
+        try (
+                ObjectOutputStream outputStream = new ObjectOutputStream(
+                        new FileOutputStream("array.dat", true));
+                ){
+            outputStream.writeObject(numbers);
+            outputStream.writeObject(strings);
+        }
+
+        try (
+                ObjectInputStream inputStream = new ObjectInputStream(
+                        new FileInputStream("array.dat"));
+                ){
+            int[] newNumbers = (int[])(inputStream.readObject());
+            String[] newStrings = (String[])(inputStream.readObject());
+
+            for (int i = 0; i < newNumbers.length; i++)
+                System.out.print(newNumbers[i] + " ");
+            System.out.println();
+
+            for (int i = 0; i < newStrings.length; i++)
+                System.out.println(newStrings[i] + " ");
+        }
+    }
+}
